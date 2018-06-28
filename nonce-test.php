f6b065cf528944c8d74ff51a7eb12e65420e3477
Dasun Edirisinghe <>
2018-06-28 15:38:59 +0530

    Initial commit with blank files

---
+++ nonce-test.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Plugin Name: NonceTest
+ * Version: 1.0
+ * Plugin URI: https://dasun.blog/noce-oop
+ * Description: Plugin that use Oop Nonce
+ * Author: dazunj
+ * Text Domain:
+ * License: GPL v3
+ */
+
